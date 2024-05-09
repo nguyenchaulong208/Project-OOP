93fef46471a95d1cbad14ce5fc7c5629bb195a31
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2024-05-10 04:13:05 +0700

    Update Project

---
+++ ProjectOOP/Repo/ILuuTruSanPham.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo
+{
+    public interface ILuuTruSanPham
+    {
+        List<SanPham> DocDanhSachSanPham();
+        void ThemSanPham(SanPham sanPham);
+    }
+}
