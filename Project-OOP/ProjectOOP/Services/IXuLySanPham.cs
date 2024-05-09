e24ce9130a95ea943284a6a3af13e0d7664fd29b
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2024-05-10 03:54:23 +0700

    update

---
+++ Project-OOP/ProjectOOP/Services/IXuLySanPham.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IXuLySanPham
+    {
+        //Hàm trả về danh sách sản phẩm
+        List<SanPham> DocDanhSachSanPham(string tuKhoa = "");
+        //Ham them san pham
+        void ThemSanPham(SanPham sanPham);
+    }
+}
